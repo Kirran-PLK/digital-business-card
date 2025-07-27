a09d3f368ee47123a2a37155008681301bbf2126
kirran <kirran231@gmail.com>
2025-07-28 01:00:02 +0530

    main content completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@
 export default function Header(){
     return(
         <header>
+            <img src="./src/images/laura.png" alt="Business Card Image" className="business-card-img" />
             <div className="container">
                 <div className="header">
-                    <img src="./src/images/laura.png" alt="Business Card Image" className="business-card-img" />
                     <h1> Laura Smith </h1>
                     <p> Frontend Developer </p>
                     <a href="laurasmith.website" className="laura-site">laurasmith.website</a>
